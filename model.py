4c21e7cf520e19c95edf8fe5c43ad241dbf06a30
wezteoh <wzhenteoh@gmail.com>
2018-03-27 23:33:58 -0400

    update

--- model.py
+++ model.py
@@ -7,24 +7,16 @@ import torch.nn.functional as F
 # 2-layer lstm with mixture of gaussian parameters as outputs
 # with skip connections
 class LSTMRandWriter(nn.Module):
-    def __init__(self, args=None, cell_size=400, num_clusters=20):
+    def __init__(self, cell_size=400, num_clusters=20):
         super(LSTMRandWriter, self).__init__()
         
-        if args:
-            self.num_clusters = args.num_clusters
-            self.cell_size = args.cell_size
-        
-        else:
-            self.num_clusters = num_clusters
-            self.cell_size = cell_size
-        
-        self.lstm = nn.LSTM(input_size = 3, hidden_size = self.cell_size,\
+        self.lstm = nn.LSTM(input_size = 3, hidden_size = cell_size,\
                                 num_layers = 1, batch_first=True)
-        self.lstm2 = nn.LSTM(input_size = self.cell_size+3, \
-                                hidden_size = self.cell_size,\
+        self.lstm2 = nn.LSTM(input_size = cell_size+3, \
+                                hidden_size = cell_size,\
                                 num_layers = 1, batch_first=True)
-        self.linear1 = nn.Linear(self.cell_size*2,\
-                                    1+ self.num_clusters*6)
+        self.linear1 = nn.Linear(cell_size*2,\
+                                    1+ num_clusters*6)
         self.tanh = nn.Tanh()
         
     def forward(self, x, prev, prev2):
@@ -35,21 +27,119 @@ class LSTMRandWriter(nn.Module):
         
         h = torch.cat([h1, h2], dim=-1) # skip connection
         params = self.linear1(h)
-        weights = F.softmax(params.narrow(-1, 0, self.num_clusters), dim=-1)
+
         
-        # MoG parameters
-        mu_1 = params.narrow(-1, self.num_clusters, self.num_clusters)
-        mu_2 = params.narrow(-1, 2*self.num_clusters, self.num_clusters)
-        log_sigma_1 = params.narrow(-1, 3*self.num_clusters, self.num_clusters)
-        log_sigma_2 = params.narrow(-1, 4*self.num_clusters, self.num_clusters)
-        rho = self.tanh(params.narrow(-1, 5*self.num_clusters, \
-                        self.num_clusters))
-        
-        # Bernoulli parameter
-        end = F.sigmoid(params.narrow(-1, 6*self.num_clusters, 1))
+        mog_params = params.narrow(-1, 0, params.size()[-1]-1)
+        pre_weights, mu_1, mu_2, log_sigma_1, log_sigma_2, pre_rho, pre_end = mog_params.chunk(6, dim=-1)
+        weights = F.softmax(pre_weights*(1+bias), dim=-1)
+        rho = self.tanh(pre_rho)
+        end = F.sigmoid(params.narrow(-1, params.size()[-1]-1, 1))
+        # 
+        # weights = F.softmax(params.narrow(-1, 0, self.num_clusters), dim=-1)
+        # 
+        # # MoG parameters
+        # mu_1 = params.narrow(-1, self.num_clusters, self.num_clusters)
+        # mu_2 = params.narrow(-1, 2*self.num_clusters, self.num_clusters)
+        # log_sigma_1 = params.narrow(-1, 3*self.num_clusters, self.num_clusters)
+        # log_sigma_2 = params.narrow(-1, 4*self.num_clusters, self.num_clusters)
+        # rho = self.tanh(params.narrow(-1, 5*self.num_clusters, \
+        #                 self.num_clusters))
+        # 
+        # # Bernoulli parameter
+        # end = F.sigmoid(params.narrow(-1, 6*self.num_clusters, 1))
         
         return end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2,\
             rho, (h1_n, c1_n), (h2_n, c2_n)
             
+# attention window for handwriting synthesis
+class Window(nn.Module):
+    def __init__(self, padded_text_len, K):
+        super(Window, self).__init__()
+        self.linear = nn.Linear(cell_size, 3*K)
+        self.padded_text_len = text_len
+        
+    def forward(self, x, kappa_old, onehots, text_lens):
+        params = self.linear(x).exp()
+        
+        alpha, beta, pre_kappa = params.chunck(3, dim=-1)
+        kappa = kappa_old + pre_kappa
+        # alpha = params.narrow(-1, 0, K).exp()
+        # beta = params.narrow(-1, K, K).exp()
+        # kappa = kappa_old + params.narrow(-1, 2*K, K).exp() 
+        
+        indices = torch.from_numpy(np.array(range(self.padded_text_len + 1))).type(torch.FloatTensor)
+        if cuda:
+            indices = indices.cuda()
+        indices = Variable(indices, requires_grad=False)
+        gravity = -beta.unsqueeze(2)*(kappa.unsqueeze(2).repeat(1, 1, self.padded_text_len + 1)-indices)**2
+        phi = (alpha.unsqueeze(2) * gravity.exp()).sum(dim=1)*(self.padded_text_len/text_lens)
+        
+        w = (phi.narrow(-1, 0, self.text_len).unsqueeze(2) * onehots).sum(dim=1) 
+        return w, kappa, phi
+
+class LSTM1(nn.Module):
+    def __init__(self, padded_text_len, vocab_len, cell_size):
+        super(LSTM1, self).__init__()
+        self.lstm = nn.LSTMCell(input_size = 3 + vocab_len, hidden_size = cell_size)
+        self.window = Window(padded_text_len)
+        
+    def forward(self, x, onehots, text_lens, w_old, kappa_old, prev):
+        h1s = []
+        ws = []
+        phis = []
+        for _ in range(x.size()[1]):
+
+            cell_input = torch.cat([x.narrow(1,_,1).squeeze(1),w_old], dim=-1)
+            prev = self.lstm(cell_input, prev)
+            
+            # attention window parameters
+            w_old, kappa_old, old_phi = self.window(prev[0], kappa_old, onehots, text_lens)
+            
+            # concatenate for single pass through the next layer
+            h1s.append(prev[0])
+            ws.append(w_old)
+        
+        return torch.stack(ws, dim=0).permute(1,0,2), torch.stack(h1s, dim=0).permute(1,0,2), \
+                prev, w_old, kappa_old, old_phi 
     
-    
+class LSTM2(nn.Module):
+    def __init__(self, vocab_len, cell_size):
+        super(LSTM2, self).__init__()
+        self.lstm = nn.LSTM(input_size=3 + vocab_len + cell_size, 
+                            hidden_size = cell_size, num_layers = 1, batch_first =True)
+        
+    def forward(self, x, ws, h1s, prev2):
+        lstm_input = torch.cat([x,ws,h1s], -1)
+        h2s, prev2 = self.lstm(lstm_input, prev2)
+        return h2s, prev2
+
+# 2-layer lstm with mixture of gaussian parameters as outputs
+# with skip connections
+class LSTMSynthesis(nn.Module):
+    def __init__(self, padded_text_len, vocab_len, cell_size=400, num_clusters=20, K=10):
+        super(LSTMSynthesis, self).__init__()
+        self.lstm1 = LSTM1(padded_text_len, vocab_len, cell_size)
+        self.lstm2 = LSTM2(vocab_len, cell_size)
+        self.linear = nn.Linear(cell_size*2, 1+ num_clusters*6)
+        self.tanh = nn.Tanh()
+        
+    def forward(self, x, onehots, text_lens, w_old, kappa_old, prev, prev2, bias=0.):
+        ws, h1s, prev, w_old, kappa_old, old_phi = self.lstm1(x, onehots, text_lens, w_old, kappa_old, prev)
+        h2s, prev2 = self.lstm2(x, ws, h1s, prev2)
+        
+        params = self.linear(torch.cat([h1s,h2s], dim=-1))
+        mog_params = params.narrow(-1, 0, params.size()[-1]-1)
+        pre_weights, mu_1, mu_2, log_sigma_1, log_sigma_2, pre_rho, pre_end = mog_params.chunk(6, dim=-1)
+        weights = F.softmax(pre_weights*(1+bias), dim=-1)
+        rho = self.tanh(pre_rho)
+        end = F.sigmoid(params.narrow(-1, params.size()[-1]-1, 1))
+        
+        # weights = F.softmax(params.narrow(-1, 0, num_clusters)*(1+bias), dim=-1)
+        # mu_1 = params.narrow(-1, num_clusters, num_clusters)
+        # mu_2 = params.narrow(-1, 2*num_clusters, num_clusters)
+        # log_sigma_1 = params.narrow(-1, 3*num_clusters, num_clusters)
+        # log_sigma_2 = params.narrow(-1, 4*num_clusters, num_clusters)
+        # rho = self.tanh(params.narrow(-1, 5*num_clusters, num_clusters))
+        # end = F.sigmoid(params.narrow(-1, 6*num_clusters, 1))
+        
+        return end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, w_old, kappa_old, prev, prev2, old_phi
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-import pickle
 import time
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -14,13 +13,12 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data
 
+# import model and utilities
 from model import *
 from utilz import *
 
-
 # find gpu
 cuda = torch.cuda.is_available()
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -41,40 +39,47 @@ def main():
     parser.add_argument('--decay_rate', type=float, default=0.99,
                         help='lr decay rate for adam optimizer per epoch')
     parser.add_argument('--num_clusters', type=int, default=20,
-                        help='number of gaussian mixture clusters')
+                        help='number of gaussian mixture clusters for stroke prediction')
+    parser.add_argument('--K', type=int, default=10,
+                        help='number of attention clusters on text input')
     args = parser.parse_args()
     
     # prepare training data
-    train_data = [np.load('train_strokes_800.npy'), np.load('train_masks_800.npy'), np.load('train_onehot_800.npy')]
+    train_data = [np.load('train_strokes_800.npy'), np.load('train_masks_800.npy'), np.load('train_onehot_800.npy'),
+                np.load('train_text_lens.npy')]
     for _ in range(len(train_data)):
         train_data[_] =torch.from_numpy(train_data[_]).type(torch.FloatTensor)
         if cuda:
             train_data[_] = train_data[_].cuda()
-    train_data = [(train_data[0][i], train_data[1][i], train_data[2][i]) for i in range(len(train_data[0]))] 
+    train_data = [(train_data[0][i], train_data[1][i], 
+                train_data[2][i], train_data[3][i]) for i in range(len(train_data[0]))] 
     train_loader = torch.utils.data.DataLoader(
-        train_data, batch_size=args.batch_size, shuffle=True, drop_last=True)
+        train_data, batch_size=bsize, shuffle=True, drop_last=True)
         
     # prepare validation data
     validation_data = [np.load('validation_strokes_800.npy'), np.load('validation_masks_800.npy'), 
-                    np.load('validation_onehot_800.npy')]
+                    np.load('validation_onehot_800.npy'), np.load('validation_text_lens.npy')]
     for _ in range(len(validation_data)):
         validation_data[_] = torch.from_numpy(validation_data[_]).type(torch.FloatTensor)
         if cuda:
             validation_data[_] = validation_data[_].cuda()
-    validation_data = [(validation_data[0][i], validation_data[1][i], validation_data[2][i]) 
+    validation_data = [(validation_data[0][i], validation_data[1][i], validation_data[2][i], validation_data[3][i]) 
                     for i in range(len(validation_data[0]))] 
     validation_loader = torch.utils.data.DataLoader(
-        validation_data, batch_size=args.batch_size, shuffle=True, drop_last=True)
+        validation_data, batch_size=bsize, shuffle=True, drop_last=True)
     
     
     # training
     if args.task == 'rand_write':
         rand_write_train(args, train_loader, validation_loader)
         
+    if args.task == 'synthesis':
+        synthesis_train(args, train_loader, validation_loader)
+        
 
 def rand_write_train(args, train_loader, validation_loader):
     # define model and optimizer
-    model = LSTMRandWriter(args)
+    model = LSTMRandWriter(args.cell_size, args.num_clusters)
     if cuda:
         model = model.cuda()
     
@@ -96,7 +101,7 @@ def rand_write_train(args, train_loader, validation_loader):
     
     for epoch in range(args.num_epochs):
         train_loss = 0
-        for batch_idx, (data, masks, onehots) in enumerate(train_loader):
+        for batch_idx, (data, masks, onehots, text_masks) in enumerate(train_loader):
             
             # gather training batch
             step_back = data.narrow(1,0,args.timesteps)
@@ -152,19 +157,18 @@ def rand_write_train(args, train_loader, validation_loader):
             best_validation_loss = validation_loss
             save_checkpoint(epoch, model, validation_loss, optimizer, args.model_dir, args.task + '_best.pt')
         
-        # learning rate annealing
-        if (epoch+1)%10 == 0:
-            optimizer = decay_learning_rate(optimizer)
+        # # learning rate annealing
+        # if (epoch+1)%10 == 0:
+        #     optimizer = decay_learning_rate(optimizer)
         
         # checkpoint model and training
-        #if (epochs+1)%5 == 0:
         filename = args.task + '_epoch_{}.pt'.format(epoch+1)
         save_checkpoint(epoch, model, validation_loss, optimizer, args.model_dir, filename)
     
-        # testing checkpoints
-        state = torch.load(os.path.join(args.model_dir, filename))
-        model.load_state_dict(state['model'])
-        optimizer.load_state_dict(state['optimizer'])
+        # # testing checkpoints
+        # state = torch.load(os.path.join(args.model_dir, filename))
+        # model.load_state_dict(state['model'])
+        # optimizer.load_state_dict(state['optimizer'])
         
         print('wall time: {}s'.format(time.time()-start_time))
         
@@ -174,6 +178,116 @@ def rand_write_train(args, train_loader, validation_loader):
     f1.savefig(args.task +"_loss_curves", bbox_inches='tight')
     
     
+def synthesis_train(args, train_loader, validation_loader):
+    # infer padded text len and vocab len
+    padded_text_len, vocab_len = train_loader.dataset[0][2].size()
+    
+    # define model and optimizer
+    model = LSTMSynthesis(padded_text_len, vocab_len, args.cell_size, args.num_clusters, args.K)
+    if cuda:
+        model = model.cuda()
+    
+    optimizer = optim.Adam([{'params':model.parameters()},], lr=args.learning_rate)
+    
+    # initialize null hidden, memory states and cluster centers
+    h1_init = c1_init = torch.zeros((args.batch_size, args.cell_size))
+    h2_init = c2_init = torch.zeros((1, args.batch_size, args.cell_size))
+    kappa_old = torch.zeros(args.batch_size, args.K)
+    
+    if cuda:
+        h1_init, c1_init = h1_init.cuda(), c1_init.cuda()
+        h2_init, c2_init = h2_init.cuda(), c2_init.cuda()
+        kappa_old = kappa_old.cuda()
+        
+    h1_init, c1_init = Variable(h1_init, requires_grad=False), Variable(c1_init, requires_grad=False)
+    h2_init, c2_init = Variable(h2_init, requires_grad=False), Variable(c2_init, requires_grad=False)
+    kappa_old = Variable(kappa_old, requires_grad=False)
+    
+    t_loss = []
+    v_loss = []
+    best_validation_loss = 1E10
+    
+    # training
+    start_time = time.time()
+    for epoch in range(args.num_epochs):
+        train_loss =0
+        for batch_idx, (data, masks, onehots, text_lens) in enumerate(train_loader):
+            
+            # gather training batch
+            step_back = data.narrow(1,0,timesteps)
+            x = Variable(step_back, requires_grad=False)
+            onehots = Variable(onehots, requires_grad = False)
+            masks = Variable(masks, requires_grad=False)
+            text_lens = Variable(text_lens, requires_grad=False)
+            
+            # focus window weight on first text char
+            w_old = onehots.narrow(1,0,1).squeeze()
+            
+            optimizer.zero_grad()
+            
+            # feed forward
+            outputs = model(x,onehots, text_lens, w_old, kappa_old, (h1_init, c1_init), (h2_init, c2_init))
+            end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, w, kappa, prev, prev2, old_phi = outputs
+            data = data.narrow(1,1,timesteps)
+            y = Variable(data, requires_grad=False)
+            loss = -log_likelihood(end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, y, masks)/torch.sum(masks)
+            loss.backward()
+            train_loss += loss.data[0]
+            optimizer.step()
+            if batch_idx % 10 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch+1, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader),
+                    loss.data[0]))
+    
+        print('====> Epoch: {} Average train loss: {:.4f}'.format(
+            epoch+1, train_loss/(len(train_loader.dataset)//bsize)))
+        t_loss.append(train_loss/(len(train_loader.dataset)//bsize))
+        
+        # validation
+        # prepare validation data
+        (validation_samples, masks, onehots, text_lens) = list(enumerate(validation_loader))[0][1]
+        step_back = validation_samples.narrow(1,0,timesteps)
+        masks = Variable(masks, requires_grad=False)
+        masks = masks.narrow(1,0,timesteps)
+        onehots = Variable(onehots, requires_grad=False)
+        text_lens = Variable(text_lens, requires_grad=False)
+        
+        w_old = onehots.narrow(1,0,1).squeeze()
+        x = Variable(step_back, requires_grad=False)
+        
+        validation_samples = validation_samples.narrow(1,1,timesteps)
+        y = Variable(validation_samples, requires_grad = False)
+    
+        outputs = model(x2, onehots, text_lens, w_old, kappa_old, (h1_init2, c1_init2), (h2_init2, c2_init2))
+        end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, w, kappa, prev, prev2, old_phi = outputs
+        loss = -log_likelihood(end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, y2, masks)/torch.sum(masks)
+        validation_loss = loss.data[0]
+        print('====> Epoch: {} Average validation loss: {:.4f}'.format(\
+            epoch+1, validation_loss))
+        v_loss.append(validation_loss)
+    
+        
+        # # learning rate annealing
+        # if (epoch+1)%10 == 0:
+        #     optimizer = decay_learning_rate(optimizer)
+        
+        # checkpoint model and training
+        filename = args.task + '_epoch_{}.pt'.format(epoch+1)
+        save_checkpoint(epoch, model, validation_loss, optimizer, args.model_dir, filename)
+    
+        # # testing checkpoints
+        # state = torch.load(os.path.join(args.model_dir, filename))
+        # model.load_state_dict(state['model'])
+        # optimizer.load_state_dict(state['optimizer'])
+        
+        print('wall time: {}s'.format(time.time()-start_time))
+        
+        f1 = plt.figure(1)
+        plt.plot(range(1, args.num_epochs+1), t_loss, color='blue', linestyle='solid')
+        plt.plot(range(1, args.num_epochs+1), v_loss, color='red', linestyle='solid')
+        f1.savefig(args.task +"_loss_curves", bbox_inches='tight')
+
 # training objective
 def log_likelihood(end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, \
                     y, masks):
@@ -201,7 +315,5 @@ def log_likelihood(end, weights, mu_1, mu_2, log_sigma_1, log_sigma_2, rho, \
     
     return (end_loglik*masks).sum() + ((mog_loglik)*masks).sum()
 
-
-    
 if __name__ == '__main__':
     main()
